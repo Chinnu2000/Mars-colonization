4c6afb6894c3a334261985c37975fdcfb5902bd8
ramyaachanta <64644325+ramyaachanta@users.noreply.github.com>
2020-07-24 20:09:01 +0530

    Update app.js

--- Mar-colonization/app.js
+++ Mar-colonization/app.js
@@ -33,6 +33,8 @@ winningLine = [[0,1,2],[0,3,6],[3,4,5],[1,4,7],[6,7,8],[2,5,8],[0,4,8],[2,4,6]];
 
 
 //audio 
+var myAudio = document.getElementById("myAudio");
+var isPlaying = false;
 function togglePlay() {
   isPlaying ? myAudio.pause() : myAudio.play();
 };
